26d8a95e8039e70587ec6aa3d4e501fe187eea0f
mouned <moussa.nedjari92@gmail.com>
2017-11-23 15:06:36 +0100

    test jenkins4

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -22,10 +22,6 @@ public class User {
         this.firstName = firstName;
     }
 
-    
-    
-    
-    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
